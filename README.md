4f3faff6c3ee1e14edc31dd0b3cc06a200c60eb7
Samanjate Sood <samanjate@gmail.com>
2018-04-21 00:36:59 -0400

    random algorithm to solve game

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 AI for the game [2048](https://github.com/gabrielecirulli/2048).
 
-Originally forked from [here](https://github.com/ovolve/2048-AI). 
+Originally forked from [ovolve](https://github.com/ovolve/2048-AI). 
 
 Visual representation of algorithm will be avaiable soon.
--- js/ai.js
+++ js/ai.js
@@ -1,151 +1,102 @@
 function AI(grid) {
-  console.log(grid);
   this.grid = grid;
-}
-
-// static evaluation function
-AI.prototype.eval = function() {
-  var emptyCells = this.grid.availableCells().length;
-
-  var smoothWeight = 0.1,
-      //monoWeight   = 0.0,
-      //islandWeight = 0.0,
-      mono2Weight  = 1.0,
-      emptyWeight  = 2.7,
-      maxWeight    = 1.0;
-
-  return this.grid.smoothness() * smoothWeight
-       //+ this.grid.monotonicity() * monoWeight
-       //- this.grid.islands() * islandWeight
-       + this.grid.monotonicity2() * mono2Weight
-       + Math.log(emptyCells) * emptyWeight
-       + this.grid.maxValue() * maxWeight;
-};
-
-// alpha-beta depth first search
-AI.prototype.search = function(depth, alpha, beta, positions, cutoffs) {
-  var bestScore;
-  var bestMove = -1;
-  var result;
-
-  // the maxing player
-  if (this.grid.playerTurn) {
-    bestScore = alpha;
-    for (var direction in [0, 1, 2, 3]) {
-      var newGrid = this.grid.clone();
-      if (newGrid.move(direction).moved) {
-        positions++;
-        if (newGrid.isWin()) {
-          return { move: direction, score: 10000, positions: positions, cutoffs: cutoffs };
-        }
-        var newAI = new AI(newGrid);
-
-        if (depth == 0) {
-          result = { move: direction, score: newAI.eval() };
-        } else {
-          result = newAI.search(depth-1, bestScore, beta, positions, cutoffs);
-          if (result.score > 9900) { // win
-            result.score--; // to slightly penalize higher depth from win
-          }
-          positions = result.positions;
-          cutoffs = result.cutoffs;
-        }
-
-        if (result.score > bestScore) {
-          bestScore = result.score;
-          bestMove = direction;
-        }
-        if (bestScore > beta) {
-          cutoffs++
-          return { move: bestMove, score: beta, positions: positions, cutoffs: cutoffs };
-        }
-      }
-    }
-  }
-
-  else { // computer's turn, we'll do heavy pruning to keep the branching factor low
-    bestScore = beta;
-
-    // try a 2 and 4 in each cell and measure how annoying it is
-    // with metrics from eval
-    var candidates = [];
-    var cells = this.grid.availableCells();
-    var scores = { 2: [], 4: [] };
-    for (var value in scores) {
-      for (var i in cells) {
-        scores[value].push(null);
-        var cell = cells[i];
-        var tile = new Tile(cell, parseInt(value, 10));
-        this.grid.insertTile(tile);
-        scores[value][i] = -this.grid.smoothness() + this.grid.islands();
-        this.grid.removeTile(cell);
-      }
-    }
-
-    // now just pick out the most annoying moves
-    var maxScore = Math.max(Math.max.apply(null, scores[2]), Math.max.apply(null, scores[4]));
-    for (var value in scores) { // 2 and 4
-      for (var i=0; i<scores[value].length; i++) {
-        if (scores[value][i] == maxScore) {
-          candidates.push( { position: cells[i], value: parseInt(value, 10) } );
-        }
-      }
-    }
-
-    // search on each candidate
-    for (var i=0; i<candidates.length; i++) {
-      var position = candidates[i].position;
-      var value = candidates[i].value;
-      var newGrid = this.grid.clone();
-      var tile = new Tile(position, value);
-      newGrid.insertTile(tile);
-      newGrid.playerTurn = true;
-      positions++;
-      newAI = new AI(newGrid);
-      result = newAI.search(depth, alpha, bestScore, positions, cutoffs);
-      positions = result.positions;
-      cutoffs = result.cutoffs;
-
-      if (result.score < bestScore) {
-        bestScore = result.score;
-      }
-      if (bestScore < alpha) {
-        cutoffs++;
-        return { move: null, score: alpha, positions: positions, cutoffs: cutoffs };
-      }
-    }
-  }
-
-  return { move: bestMove, score: bestScore, positions: positions, cutoffs: cutoffs };
 }
 
 // performs a search and returns the best move
 AI.prototype.getBest = function() {
-  return this.iterativeDeep();
+  return getBestMove(this.grid, 100);  
 }
 
-// performs iterative deepening over the alpha-beta search
-AI.prototype.iterativeDeep = function() {
-  var start = (new Date()).getTime();
-  var depth = 0;
-  var best;
-  do {
-    var newBest = this.search(depth, -10000, 10000, 0 ,0);
-    if (newBest.move == -1) {
-      break;
-    } else {
-      best = newBest;
-    }
-    depth++;
-  } while ( (new Date()).getTime() - start < minSearchTime);
-  return best
-}
-
-AI.prototype.translate = function(move) {
+function moveName(move) {
  return {
     0: 'up',
     1: 'right',
     2: 'down',
     3: 'left'
   }[move];
+}
+
+var global_max_score;
+var global_max_score_moves;
+
+function getBestMove(grid, runs, debug) {
+    var bestScore = 0; 
+    var bestMove = -1;
+
+    for (var i=0;i<4;i++) {
+      // score move position
+      var res = multiRandomRun(grid, i, runs);
+      var score = res.score;
+      
+      if (score >= bestScore) {
+        bestScore = score;
+        bestMove = i;
+        bestAvgMoves = res.avg_moves;
+      }
+    }
+    if(!grid.movesAvailable()) console.log('bug2');   
+    // assert move found    
+    if (bestMove == -1) {
+      console.log('ERROR...'); 
+      errorGrid = grid.clone();
+    }  
+    
+    return {move: bestMove, score: bestScore};
+}
+
+function multiRandomRun(grid, move, runs) {
+  var total = 0.0;
+  var min = 1000000;
+  var max = 0;
+  var total_moves = 0;
+  
+  for (var i=0 ; i < runs ; i++) {
+    var res = randomRun(grid, move);
+    var s = res.score;
+    if (s == -1) return -1;
+      
+    total += s;
+    total_moves += res.moves;
+    if (s < min) min = s;
+    if (s > max) max = s;
+  }
+  
+  var avg = total / runs;
+  var avg_moves = total_moves / runs;
+
+//  return max;
+//  return min;
+//  return avg+max;
+  return {score: avg, avg_moves:avg_moves};
+}
+
+function randomRun(grid, move) {  
+  var g = grid.clone();
+  var score = 0;
+  var res = moveAndAddRandomTiles(g, move);
+  if (!res.moved) {
+    return -1; // can't start
+  } 
+  score += res.score;
+
+  // run til we can't
+  var moves=1;
+  while (true) {
+    if (!g.movesAvailable()) break;
+    
+    var res = g.move(Math.floor(Math.random() * 4));
+    if (!res.moved) continue;
+    
+    score += res.score;
+    g.addRandomTile();
+    moves++;
+  }
+  // grid done.
+  return {score:score, moves:moves};
+}
+
+function moveAndAddRandomTiles(grid, direction) {
+  var res = grid.move(direction);
+  if (res.moved) grid.addRandomTile();
+  return res;
 }
